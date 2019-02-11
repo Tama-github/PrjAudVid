b4fa2d982f25dc6d108a51c21bf032a2abc7331c
Tama-github <ludovic.burg@univ-tlse3.fr>
2019-02-11 13:26:52 +0100

    test added

--- test.py
+++ test.py
@@ -1,7 +1,23 @@
 from Vid.Aud.soundgenerator import SoundGenerator 
+import numpy as np
+from scipy.io.wavfile import write as wavwrite
+from scipy.io.wavfile import read as wavread
+import matplotlib.pyplot as plt
+from scipy import signal
+from scipy import stats
+import scipy.fftpack as fftp
+import pylab as py
+import math
+from audiolab import play
+import sys
+import time
 
-s = SoundGenerator('ressources/lb_idle.wav')
+sound = SoundGenerator("ressources/lb_idle.wav")
 
-print (s.fs)
-print (s.data)
+fs,s = wavread("ressources/lb_idle.wav")
+
+s = sound.scale(s,-1.,1)
+s = sound.sound2ChanelsWlength(s,fs)
+
+play(s,fs)
 
